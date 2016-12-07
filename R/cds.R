3a716374006895f183ac139d7d500e603991e7f2
Charlotte Wickham <cwickham@gmail.com>
2016-12-07 09:44:02 -0800

    scaffold for docs

---
+++ R/cds.R
@@ -0,0 +1,40 @@
+#' NASS Crashworthiness Data System
+#'
+#' The CDS blah blah blah
+#' @format something about being tibbles
+#' @source \url{http://www.nhtsa.gov/Data/National-Automotive-Sampling-System-(NASS)/NASS-Crashworthiness-Data-System}
+#' @name cds
+NULL
+
+#' @rdname cds
+"cds_acc_desc"
+
+#' @rdname cds
+"cds_accident"
+
+#' @rdname cds
+"cds_event"
+
+#' @rdname cds
+"cds_gv"
+
+#' @rdname cds
+"cds_oa"
+
+#' @rdname cds
+"cds_oi"
+
+#' @rdname cds
+"cds_pers_pro"
+
+#' @rdname cds
+"cds_typ_acc"
+
+#' @rdname cds
+"cds_ve"
+
+#' @rdname cds
+"cds_veh_pro"
+
+#' @rdname cds
+"cds_vi"
---
+++ R/nass.R
@@ -0,0 +1,17 @@
+#' nass: Data from the National Automotive Sampling System (NASS)
+#'
+#' Data from both the General Estimates System (GES) and
+#' Crashworthiness Data System (CDS) which are representive random samples of
+#' crashes reported by the National Highway Traffic Safety Administration.
+#'
+#' @section CDS:
+#' The Crashworthiness Data System data sets are all have the prefix \code{cds_}.
+#' See \code{\link{cds}}.  Currently only 2015.
+#'
+#'
+#' @section GES:
+#' General Estimates System data isn't in this package yet
+#'
+#' @docType package
+#' @name nass
+NULL
